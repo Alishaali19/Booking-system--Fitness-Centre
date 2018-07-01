61451bcc6775e97dc9a3d19df53488763d94451f
Alisha <alishaali16@outlook.com>
2018-06-30 20:56:56 -0400

    homepage
    added 2 headings to the homepage.

--- Homepage.php
+++ Homepage.php
@@ -4,10 +4,12 @@
 	<title>Transformation Fitness Centre</title>
 </head>
 <body>
-		<div>
-			
+		<div> <center>
+			<h2> WELCOME TO TRANSFORMATION FITNESS CENTRE </h2>
+
+			<h3> Transform your life, fitness and health! </h3>
 				
-		
+		</center>
 		</div>
 
 </body>
