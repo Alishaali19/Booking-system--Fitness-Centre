c786af4ad9033a9287b7cf7bb87c236ced17542b
Alisha <alishaali16@outlook.com>
2019-02-08 03:31:26 -0400

    Created pricing page
    added layout for pricing page

--- Header.php
+++ Header.php
@@ -12,12 +12,12 @@
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">Transformation Fitness Centre</a>
+      <a class="navbar-brand" href="Homepage.php">Transformation <br> Fitness <br> Centre</a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
+      <li class="active"><a href="Homepage.php">Home</a></li>
 
-      			<li><a href="Homepage.php"> Home </a></li>
+      	<li><a href="Pricing.php"> Pricing </a></li>
 				<li><a href="Classes.php"> Classes </a></li>
 				<li><a href="Features.php"> Features </a></li>
 				<li><a href="Registration.php"> Register</a></li>
@@ -35,3 +35,6 @@
 
 </body>
 </html>
+
+
+
---
+++ Pricing.php
@@ -0,0 +1,76 @@
+<?php include "header.php" ?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pricing</title>
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+</head>
+
+
+<body>
+
+	<h1>Packages</h1> 
+
+<div class="container">
+    			<div class="row pricing">
+						<div class="col-md-4">
+						<div class="well">
+							<h3><b>Plan 4</b></h3>
+							<hr>
+							<p>4 GB Datatransfer</p>
+							<hr>
+							<p>4 GB Storage</p>
+							<hr>
+							<p><b>€ 5,99 /PM</b></p>
+							<hr>
+							<a href="#" class="btn btn-success btn-block">Sign Up</a>
+						</div>
+					</div>
+
+					
+						<div class="col-md-4">
+						<div class="well">
+							<h3><b>Plan 4</b></h3>
+							<hr>
+							<p>4 GB Datatransfer</p>
+							<hr>
+							<p>4 GB Storage</p>
+							<hr>
+							<p><b>€ 5,99 /PM</b></p>
+							<hr>
+							<a href="#" class="btn btn-success btn-block">Sign Up</a>
+						</div>
+					</div>
+
+
+
+
+
+
+
+				</div>
+			</div>
+
+
+
+
+
+</body>
+
+
+</html>
+
+
+
+
+<?php include "Footer.php" ?>
+
+
+
